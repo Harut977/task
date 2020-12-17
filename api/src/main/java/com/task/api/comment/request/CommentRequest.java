718a7a35deba1da0cff9bb7ff1bafc7b16ad59a7
Harut <harut.aristakesyan@gmail.com>
2020-12-17 23:44:25 +0400

    project finished

--- api/src/main/java/com/task/api/comment/request/CommentRequest.java
+++ api/src/main/java/com/task/api/comment/request/CommentRequest.java
@@ -3,6 +3,8 @@ package com.task.api.comment.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * This represents the request for the comments, which includes a String of the comment.
@@ -15,5 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CommentRequest {
 
+    @NotNull(message = "Comment han not be null")
+    @NotBlank(message = "Comment han not be blank")
     private String comment;
 }
